4e80a932d06ff25f8b293b13ef650d88d2cb57b4
Jack <254184504+im-ashey@users.noreply.github.com>
2026-05-28 12:06:39 -0700

    adds two arrays and prints them

--- src/main.c
+++ src/main.c
@@ -1,7 +1,29 @@
 #include <stdio.h>
 
 int main() {
-  printf("Hello, World.");
+	char users[3][6] = {"user1", "user2", "admin"};
+	char passwords[3][13] = {"goodpassword", "badpassword", "adminpassword"};
 
-  return 0;
+   	for (int i = 0; i < 3; i++) { // this syntax looks just like javascript! :)
+   		printf(users[i]);
+     	printf(":");
+      	printf(passwords[i]);
+      	printf("\n");
+    }
+
+   	return 0;
 }
+
+
+/*
+*
+
+char arr_name[r][m] = {s1, s2, ...., sn};
+
+arr_name: Name of the variable.
+r: Maximum number of strings to be stored in the array
+m: Maximum number of character values that can be stored in each string.
+s1, s2, ...., sn: Strings to be stored.
+
+*
+*/
